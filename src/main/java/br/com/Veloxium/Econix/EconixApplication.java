96ec6f34d877604f9ee6ac179b8c3071e42d25fe
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-06-01 15:42:39 -0300

    initializr

---
+++ src/main/java/br/com/Veloxium/Econix/EconixApplication.java
@@ -0,0 +1,13 @@
+package br.com.Veloxium.Econix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Veloxium/Econix/EconixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Veloxium.Econix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
